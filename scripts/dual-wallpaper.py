0d37669348d6e1485251b91714781f085a075f0f
Fernando Martins <me@fernandomartins.net>
2016-04-26 12:04:09 +0100

    updated dual scripts

--- scripts/dual-wallpaper.py
+++ scripts/dual-wallpaper.py
@@ -4,7 +4,7 @@ import glob
 import random
 import os
 
-files = glob.glob("/home/fmm/Wallpapers/*.jpg")
+files = glob.glob("/home/fmm/wallpapers/*.jpg")
 random.shuffle(files)
 command = "feh --no-fehbg --bg-fill " + files[0] + " " + files[1]
 os.system(command)
--- scripts/dual.sh
+++ scripts/dual.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 xrandr --output HDMI1 --rotate right --left-of HDMI2
-python $HOME/Scripts/dual-wallpaper.py
+python $HOME/scripts/dual-wallpaper.py
